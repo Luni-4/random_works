5700f3b46a28fe431a31ba9b37d743913e2c92f4
Luni-4 <luni-4@hotmail.it>
2018-09-30 19:12:12 +0200

    Add licenseheaders project
    This project adds project, copyright and license headers to all files of any programming language

---
+++ script/licenseheaders/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""Packaging script."""
+
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+readme = open(os.path.join(here, 'README.rst')).read()
+
+setup(
+    name="licenseheaders",
+    version="0.3",
+    author="Johann Petrak",
+    author_email="johann.petrak@gmail.com",
+    description='Add or change license headers for all files in a directory',
+    license="MIT",
+    keywords="",
+    url="http://github.com/johann-petrak/licenseheaders",
+    py_modules=['licenseheaders'],
+    packages=[''],
+    package_data={'': ['templates/*']},
+    include_package_data=True,
+    entry_points={'console_scripts': ['licenseheaders=licenseheaders:main']},
+    long_description=readme,
+    # test_suite='tests',
+    setup_requires=[],
+    # tests_require=['mock'],
+    classifiers=["Development Status :: 5 - Production/Stable",
+                 "License :: OSI Approved :: MIT License",
+                 "Environment :: Console",
+                 "Natural Language :: English",
+                 "Programming Language :: Python :: 2.7",
+                 "Programming Language :: Python :: 3.3",
+                 "Programming Language :: Python :: 3.4",
+                 "Programming Language :: Python :: 3.5",
+                 "Topic :: Software Development",
+                 "Topic :: Software Development :: Code Generators",
+                 "Intended Audience :: Developers",
+                ],
+)
